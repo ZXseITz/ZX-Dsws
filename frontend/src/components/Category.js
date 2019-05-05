1fdf74d8a9d6f5a2b165dd7c3079bb60ddae4068
Claudio Seitz <zxseitz@gmail.com>
2019-05-05 20:17:51 +0200

    removed react bootstrap from category

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,14 +1,13 @@
 import React, {Component} from "react"
-import {Button, Modal, Form, Table, Row} from "react-bootstrap"
-import CategoryModel from "./CategoryModel"
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import config from "../config.json"
 
 export default class Category extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            model: {},
-            newModel: {},
             categories: []
         };
 
@@ -37,35 +36,33 @@ export default class Category extends Component {
         });
     };
 
-    createCategory = () => {
+    createCategory = (category) => {
         fetch(`http://${config.host}/api/categories`, {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json",
             },
-            body: this.stringifyCategory(this.state.newModel)
+            body: this.stringifyCategory(category)
         })
             .then(() => console.log(`created category successfully`))
             .then(() => this.loadCategories())
             .catch(err => console.error(err))
     };
 
-    updateCategory = () => {
-        const id = this.state.model._id;
+    updateCategory = (id, category) => {
         fetch(`http://${config.host}/api/categories/${id}`, {
             method: 'PUT',
             headers: {
                 "Content-Type": "application/json",
             },
-            body: this.stringifyCategory(this.state.model)
+            body: this.stringifyCategory(category)
         })
             .then(() => console.log(`updated category ${id} successfully`))
             .then(() => this.loadCategories())
             .catch(err => console.error(err))
     };
 
-    deleteCategory = () => {
-        const id = this.state.model._id;
+    deleteCategory = (id) => {
         fetch(`http://${config.host}/api/categories/${id}`, {
             method: 'DELETE'
         })
@@ -77,14 +74,14 @@ export default class Category extends Component {
     componentDidMount = () => this.loadCategories();
 
     render() {
-        const model = this.state.model;
-        const newModel = this.state.newModel;
         const rows = [];
         this.state.categories.forEach(item => {
             rows.push(<tr key={item._id} onClick={() => {
-                this.setState({
-                    model: item
-                });
+                $("#modify-id").val(item._id);
+                $("#modify-age").val(item.age);
+                $("#modify-sex").val(item.sex);
+                $("#modify-distance").val(item.distance);
+                $("#modal-modify").modal();
             }}>
                 <td>{item.name}</td>
                 <td>{item.age}</td>
@@ -95,61 +92,105 @@ export default class Category extends Component {
         return (
             <div>
                 {/*Create*/}
-                <Modal show={Object.entries(newModel).length > 0} onHide={() => this.setState({newModel: {}})}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Create new category</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <CategoryModel model={newModel} />
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={() => this.setState({newModel: {}})}>
-                            Close
-                        </Button>
-                        <Button variant="primary" onClick={() => {
-                            this.createCategory();
-                            this.setState({newModel: {}})
-                        }}>
-                            Create
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+                <div className="modal fade" id="modal-create" tabIndex="-1" role="dialog"
+                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <div>
+                                    <div className="form-group">
+                                        <label>Alter</label>
+                                        <input id="create-age" className="form-control" type="text" defaultValue="7"/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Geschlecht</label>
+                                        <select id="create-sex" className="form-control">
+                                            <option>m</option>
+                                            <option>w</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Distanz</label>
+                                        <input id="create-distance" className="form-control" type="text"
+                                               defaultValue="50"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Schliessen
+                                </button>
+                                <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => {
+                                    this.createCategory({
+                                        age: $("#create-age").val(),
+                                        sex: $("#create-sex").val(),
+                                        distance: $("#create-distance").val()
+                                    });
+                                }}>Hinzufügen
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 {/*Update / Delete*/}
-                <Modal show={Object.entries(model).length > 0} onHide={() => this.setState({model: {}})}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>Update category {model.name}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <CategoryModel model={model} />
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={() => this.setState({model: {}})}>
-                            Close
-                        </Button>
-                        <Button variant="danger" onClick={() => {
-                            this.deleteCategory();
-                            this.setState({model: {}})
-                        }}>
-                            Delete
-                        </Button>
-                        <Button variant="primary" onClick={() => {
-                            this.updateCategory();
-                            this.setState({model: {}})
-                        }}>
-                            Update
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+                <div className="modal fade" id="modal-modify" tabIndex="-1" role="dialog"
+                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                                <input id="modify-id" type="hidden"/>
+                                <div className="form-group">
+                                    <label>Alter</label>
+                                    <input id="modify-age" className="form-control" type="text"/>
+                                </div>
+                                <div className="form-group">
+                                    <label>Geschlecht</label>
+                                    <select id="modify-sex" className="form-control">
+                                        <option>m</option>
+                                        <option>w</option>
+                                    </select>
+                                </div>
+                                <div className="form-group">
+                                    <label>Distanz</label>
+                                    <input id="modify-distance" className="form-control" type="text"/>
+                                </div>
+                            </div>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Schliessen
+                                </button>
+                                <button type="button" className="btn btn-danger" data-dismiss="modal" onClick={() => {
+                                    this.deleteCategory($("#modify-id").val());
+                                }}>Löschen</button>
+                                <button type="button" className="btn btn-primary" data-dismiss="modal" onClick={() => {
+                                    this.updateCategory($("#modify-id").val(), {
+                                        age: $("#modify-age").val(),
+                                        sex: $("#modify-sex").val(),
+                                        distance: $("#modify-distance").val()
+                                    });
+                                }}>Aktualisieren</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                <Button variant="outline-primary" onClick={() => this.setState({
-                    newModel: {
-                        year: 7,
-                        sex: 'm',
-                        distance: 50
-                    }
-                })} >Hinzufügen</Button>
+                <button type="button" className="btn btn-primary" onClick={() => {
+                    $('#modal-create').modal()
+                }}>
+                    Hinzufügen
+                </button>
 
-                <Table bordered hover>
+                <table className="table">
                     <thead>
                     <tr>
                         <th>Name</th>
@@ -161,8 +202,9 @@ export default class Category extends Component {
                     <tbody>
                     {rows}
                     </tbody>
-                </Table>
+                </table>
             </div>
         );
-    };
+    }
+    ;
 }
